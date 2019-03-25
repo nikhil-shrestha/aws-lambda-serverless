b24719965ffe6d7e37003af69621f391837051aa
illusionist <nikhil.shrestha1995@gmail.com>
2019-03-25 19:39:37 +0545

    updated

--- handler.js
+++ handler.js
@@ -29,7 +29,4 @@ module.exports.graphql = async (event, context) => {
       statusCode: 500
     };
   }
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
